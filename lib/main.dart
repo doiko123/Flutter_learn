bbe8b1def900c927dc03f7710275fbcbb178e226
doiko123 <ayanottyz@gmail.com>
2021-07-07 22:35:49 +0900

    subpageのclassを追加

--- lib/main.dart
+++ lib/main.dart
@@ -92,3 +92,39 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class SubPage extends StatefulWidget {
+  SubPage({Key? key, required this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _SubPage createState() => _SubPage();
+}
+
+class _SubPage extends State<SubPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        body: Builder(builder: (BuildContext context) {
+          return ElevatedButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text('戻る'),
+          );
+        }));
+  }
+}

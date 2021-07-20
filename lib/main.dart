10f33548f84afec38d5c689d19e90263ae78b97b
doiko123 <ayanottyz@gmail.com>
2021-07-21 00:16:56 +0900

    container widgetを使ってcontainerのサイズ指定とmargin指定をつけた

--- lib/main.dart
+++ lib/main.dart
@@ -62,13 +62,30 @@ class SubPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Doiko Sub Page'),
+      appBar: AppBar(
+        title: const Text('Flutter Doiko Sub Page'),
+      ),
+      body: Container(
+        height: 400.0,
+        margin: const EdgeInsets.all(16.0),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: Colors.pink,
+            width: 8.0,
+          ),
         ),
-        body: Center(
-            child: ElevatedButton(
-          onPressed: () => Navigator.of(context).pop(),
-          child: const Text('戻る'),
-        )));
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('戻る'),
+            ),
+            const Text('いってらっしゃい'),
+            const Text('おなかすいた')
+          ]
+        )
+      )
+    );
   }
 }

ffcddfe87700b05f01f52859a6ab9663774fa231
doiko123 <ayanottyz@gmail.com>
2021-08-03 22:28:31 +0900

    floating action buttonクリックでcounterの数値を増加させられるようにした

--- lib/main.dart
+++ lib/main.dart
@@ -22,76 +22,85 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Row(children: [
+          title: Row(
+        children: <Widget>[
           const Icon(Icons.create),
           const Text('Flutter Doiko Home Page'),
-        ],)
-      ),
+        ],
+      )),
       body: Center(
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return SubPage();
-                    },
-                    fullscreenDialog: true,
-                  ),
-                );
-              },
-              child: const Text('Subページへ'),
-            ),
-            const Text('あああ'),
-            const Text('いいい')
-          ]
-        ),
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) {
+                        return SubPage();
+                      },
+                      fullscreenDialog: true,
+                    ),
+                  );
+                },
+                child: const Text('Subページへ'),
+              ),
+              Text('$_counter'),
+              const Text('いいい')
+            ]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => {print("押したね？")},
-        child: const Icon(Icons.timer)),
+          onPressed: _incrementCounter,
+          tooltip: 'increment',
+          child: const Icon(Icons.add)),
     );
   }
 }
 
 class SubPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Doiko Sub Page'),
-      ),
-      body: Container(
-        height: 400.0,
-        margin: const EdgeInsets.all(16.0),
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.pink,
-            width: 8.0,
-          ),
+        appBar: AppBar(
+          title: const Text('Flutter Doiko Sub Page'),
         ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            ElevatedButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: const Text('戻る'),
+        body: Container(
+            height: 400.0,
+            margin: const EdgeInsets.all(16.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.pink,
+                width: 8.0,
+              ),
             ),
-            const Text('いってらっしゃい'),
-            const Text('おなかすいた')
-          ]
-        )
-      )
-    );
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  ElevatedButton(
+                    onPressed: () => Navigator.of(context).pop(),
+                    child: const Text('戻る'),
+                  ),
+                  const Text('いってらっしゃい'),
+                  const Text('おなかすいた')
+                ])));
   }
 }
